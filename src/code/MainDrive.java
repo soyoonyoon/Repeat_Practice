c336395ee64be35d44e8f1b147d0977495542788
soyoonyoon <starry55@naver.com>
2019-01-26 11:04:51 +0900

    세로 줄바꿈 for문 작성

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -12,7 +12,24 @@ public class MainDrive {
 //		12345
 //		12345
 		
+//		for (int j = 0; j < 5; j++) {
+//			for (int i= 0; i<5; i++) {
+//			
+//				System.out.print(i+1);
+//			
+//			}
+//			System.out.println();
+//			
+//		}
 		
+		
+//		바깥에 위치하는 for문 => 상대적으로 느리게 반복.
+//		문제에서는 가로세로 중 => 세로 줄바꿈을 담당!
+		for (int i = 0; i < 5; i++) {
+			
+			for
+			
+		}
 		
 		
 	}
